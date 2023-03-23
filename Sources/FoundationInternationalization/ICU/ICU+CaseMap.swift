6e4c75c14339c187b8d5bc2581c1b1d3f26468b6
I-Ting Tina Liu <iting_liu@apple.com>
2023-03-23 11:50:07 -0700

    Work towards: rdar://107140336 (FoundationPreview: Batch move string API)
    Move case mapping API to FoundationEssentials and FoundationInternationalization:
    - Essentials: Unlocalized caplitalized
    - Internationalization: Localized caplitalized, lowercased, uppercased
    
    Note that in the tests, we test the internal implementation instead of the public function. This is because these functions are declared as public function of
    `extension StringProtocol`, so there are two declarations on Darwin: once in FoundationPreview and once in system SDK. There is currently no way to specify which one is intended when building the package on Darwin.

---
+++ Sources/FoundationInternationalization/ICU/ICU+CaseMap.swift
@@ -0,0 +1,90 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+@_implementationOnly import FoundationICU
+
+extension ICU {
+    final class CaseMap : @unchecked Sendable {
+        let casemap: OpaquePointer
+        
+        let lock: LockedState<Void>
+        
+        // Empty locale ("") means root locale
+        init(localeID: String) throws {
+            var status = U_ZERO_ERROR
+            casemap = ucasemap_open(localeID, UInt32(), &status)
+            try status.checkSuccess()
+            
+            lock = LockedState()
+        }
+
+        deinit {
+            ucasemap_close(casemap)
+        }
+
+        private static let _cache: LockedState<[String : CaseMap]> = LockedState(initialState: [:])
+        
+        // Create and cache a new case mapping object for the specified locale
+        internal static func caseMappingForLocale(_ localeID: String?) -> CaseMap? {
+            let localeID = localeID ?? ""
+            
+            if let cached = _cache.withLock({ cache in cache[localeID] }) {
+                return cached
+            }
+            
+            guard let new = try? CaseMap(localeID: localeID) else {
+                return nil
+            }
+            
+            _cache.withLock { cache in
+                cache[localeID] = new
+            }
+            
+            return new
+        }
+
+        func lowercase(_ s: String) -> String? {
+            s.utf8CString.withUnsafeBufferPointer { srcBuf in
+                _withResizingCharBuffer { destBuf, destSize, status in
+                    ucasemap_utf8ToLower(casemap, destBuf, destSize, srcBuf.baseAddress!, Int32(srcBuf.count), &status)
+                }
+            }
+        }
+
+        func uppercase(_ s: String) -> String? {
+            s.utf8CString.withUnsafeBufferPointer { srcBuf in
+                _withResizingCharBuffer { destBuf, destSize, status in
+                    ucasemap_utf8ToUpper(casemap, destBuf, destSize, srcBuf.baseAddress!, Int32(srcBuf.count), &status)
+                }
+            }
+        }
+
+        func titlecase(_ s: String) -> String? {
+            // `ucasemap_utf8ToTitle` isn't thread-safe
+            lock.withLock {
+                s.utf8CString.withUnsafeBufferPointer { srcBuf in
+                    _withResizingCharBuffer { destBuf, destSize, status in
+                        ucasemap_utf8ToTitle(casemap, destBuf, destSize, srcBuf.baseAddress!, Int32(srcBuf.count), &status)
+                    }
+                }
+            }
+        }
+
+        func foldcase(_ s: String) -> String? {
+            s.utf8CString.withUnsafeBufferPointer { srcBuf in
+                _withResizingCharBuffer { destBuf, destSize, status in
+                    ucasemap_utf8FoldCase(casemap, destBuf, destSize, srcBuf.baseAddress!, Int32(srcBuf.count), &status)
+                }
+            }
+        }
+    }
+}
