94e4d69551a061764019f2f8e06924b078013593
Jeremy Schonfeld <jschonfeld@apple.com>
2023-04-25 15:14:41 -0700

    Fix 5.9 toolchain requirement for Xcode builds

---
+++ Sources/FoundationEssentials/FoundationEssentials.swift
@@ -0,0 +1,14 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+#if compiler(<5.9)
+#error("Building FoundationPreview requires a Swift 5.9 toolchain")
+#endif
