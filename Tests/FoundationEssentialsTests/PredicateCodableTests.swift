43480a4cfe659aa8012307b00250a218fc167502
Jeremy Schonfeld <jschonfeld@apple.com>
2023-04-25 14:39:55 -0700

    (103142402) Add unit tests

--- Tests/FoundationEssentialsTests/PredicateCodableTests.swift
+++ Tests/FoundationEssentialsTests/PredicateCodableTests.swift
@@ -152,16 +152,16 @@ final class PredicateCodableTests: XCTestCase {
         }()
     }
     
-    private struct Wrapper<Input, ConfigurationProvider : PredicateCodingConfigurationProviding> : Codable {
-        let predicate: Predicate<Input>
+    private struct Wrapper<ConfigurationProvider : PredicateCodingConfigurationProviding, each Input> : Codable {
+        let predicate: Predicate<repeat each Input>
         
-        init(_ predicate: Predicate<Input>, configuration: ConfigurationProvider.Type) {
+        init(_ predicate: Predicate<repeat each Input>, configuration: ConfigurationProvider.Type) {
             self.predicate = predicate
         }
         
         init(from decoder: Decoder) throws {
             var container = try decoder.unkeyedContainer()
-            predicate = try container.decode(Predicate<Input>.self, configuration: ConfigurationProvider.self)
+            predicate = try container.decode(Predicate<repeat each Input>.self, configuration: ConfigurationProvider.self)
         }
         
         func encode(to encoder: Encoder) throws {
@@ -171,27 +171,27 @@ final class PredicateCodableTests: XCTestCase {
     }
     
     @discardableResult
-    private func _encodeDecode<Input, EncodingConfigurationProvider: PredicateCodingConfigurationProviding, DecodingConfigurationProvider: PredicateCodingConfigurationProviding>(_ predicate: Predicate<Input>, encoding encodingConfig: EncodingConfigurationProvider.Type, decoding decodingConfig: DecodingConfigurationProvider.Type) throws -> Predicate<Input> {
+    private func _encodeDecode<EncodingConfigurationProvider: PredicateCodingConfigurationProviding, DecodingConfigurationProvider: PredicateCodingConfigurationProviding, each Input>(_ predicate: Predicate<repeat each Input>, encoding encodingConfig: EncodingConfigurationProvider.Type, decoding decodingConfig: DecodingConfigurationProvider.Type) throws -> Predicate<repeat each Input> {
         let encoder = JSONEncoder()
         let data = try encoder.encode(Wrapper(predicate, configuration: encodingConfig))
         let decoder = JSONDecoder()
-        return try decoder.decode(Wrapper<Input, DecodingConfigurationProvider>.self, from: data).predicate
+        return try decoder.decode(Wrapper<DecodingConfigurationProvider, repeat each Input>.self, from: data).predicate
     }
     
     @discardableResult
-    private func _encodeDecode<Input, ConfigurationProvider: PredicateCodingConfigurationProviding>(_ predicate: Predicate<Input>, for configuration: ConfigurationProvider.Type) throws -> Predicate<Input> {
+    private func _encodeDecode<ConfigurationProvider: PredicateCodingConfigurationProviding, each Input>(_ predicate: Predicate<repeat each Input>, for configuration: ConfigurationProvider.Type) throws -> Predicate<repeat each Input> {
         let encoder = JSONEncoder()
         let data = try encoder.encode(Wrapper(predicate, configuration: configuration))
         let decoder = JSONDecoder()
-        return try decoder.decode(Wrapper<Input, ConfigurationProvider>.self, from: data).predicate
+        return try decoder.decode(Wrapper<ConfigurationProvider, repeat each Input>.self, from: data).predicate
     }
     
     @discardableResult
-    private func _encodeDecode<Input>(_ predicate: Predicate<Input>) throws -> Predicate<Input> {
+    private func _encodeDecode<each Input>(_ predicate: Predicate<repeat each Input>) throws -> Predicate<repeat each Input> {
         let encoder = JSONEncoder()
         let data = try encoder.encode(predicate)
         let decoder = JSONDecoder()
-        return try decoder.decode(Predicate<Input>.self, from: data)
+        return try decoder.decode(Predicate<repeat each Input>.self, from: data)
     }
     
     func testBasicEncodeDecode() throws {
@@ -424,7 +424,7 @@ final class PredicateCodableTests: XCTestCase {
         func _malformedDecode<T: PredicateCodingConfigurationProviding>(_ json: String, config: T.Type = StandardConfig.self, reason: String, file: StaticString = #file, line: UInt = #line) {
             let data = Data(json.utf8)
             let decoder = JSONDecoder()
-            XCTAssertThrowsError(try decoder.decode(Wrapper<Object, T>.self, from: data), file: file, line: line) {
+            XCTAssertThrowsError(try decoder.decode(Wrapper<T, Object>.self, from: data), file: file, line: line) {
                 XCTAssertTrue(String(describing: $0).contains(reason), "Error '\($0)' did not contain reason '\(reason)'", file: file, line: line)
             }
         }
@@ -435,9 +435,9 @@ final class PredicateCodableTests: XCTestCase {
             [
               [
                 {
-                  "variable" : {
+                  "variable" : [{
                     "key" : 0
-                  },
+                  }],
                   "expression" : 0,
                   "structure" : "Swift.Int"
                 }
@@ -453,9 +453,9 @@ final class PredicateCodableTests: XCTestCase {
             [
               [
                 {
-                  "variable" : {
+                  "variable" : [{
                     "key" : 0
-                  },
+                  }],
                   "expression" : 0,
                   "structure" : "PredicateExpressions.Conjunction"
                 }
@@ -471,9 +471,9 @@ final class PredicateCodableTests: XCTestCase {
             [
               [
                 {
-                  "variable" : {
+                  "variable" : [{
                     "key" : 0
-                  },
+                  }],
                   "expression" : 0,
                   "structure" : {
                     "identifier": "PredicateExpressions.Conjunction",
@@ -495,9 +495,9 @@ final class PredicateCodableTests: XCTestCase {
             [
               [
                 {
-                  "variable" : {
+                  "variable" : [{
                     "key" : 0
-                  },
+                  }],
                   "expression" : 0,
                   "structure" : "PredicateExpressions.TestNonStandardExpression"
                 }
@@ -507,6 +507,40 @@ final class PredicateCodableTests: XCTestCase {
             config: TestExpressionConfig.self,
             reason: "This expression is unsupported by this predicate"
         )
+    }
+    
+    func testBasicVariadic() throws {
+        let predicate = Predicate<Object, Object> {
+            // $0.a == 2 && $1.a == 3
+            PredicateExpressions.build_Conjunction(
+                lhs: PredicateExpressions.build_Equal(
+                    lhs: PredicateExpressions.build_KeyPath(
+                        root: $0,
+                        keyPath: \.a
+                    ),
+                    rhs: PredicateExpressions.build_Arg(2)
+                ),
+                rhs: PredicateExpressions.build_Equal(
+                    lhs: PredicateExpressions.build_KeyPath(
+                        root: $1,
+                        keyPath: \.a
+                    ),
+                    rhs: PredicateExpressions.build_Arg(2)
+                )
+            )
+        }
+        
+        let decoded = try _encodeDecode(predicate, for: StandardConfig.self)
+        var object = Object.example
+        let object2 = Object.example
+        XCTAssertEqual(try predicate.evaluate(object, object2), try decoded.evaluate(object, object2))
+        object.a = 2
+        XCTAssertEqual(try predicate.evaluate(object, object2), try decoded.evaluate(object, object2))
+        object.a = 3
+        XCTAssertEqual(try predicate.evaluate(object, object2), try decoded.evaluate(object, object2))
+        
+        XCTAssertThrowsError(try _encodeDecode(predicate, for: EmptyConfig.self))
+        XCTAssertThrowsError(try _encodeDecode(predicate))
     }
 }
 
