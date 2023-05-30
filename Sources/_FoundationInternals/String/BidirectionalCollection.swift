e68c2b1c8dbfb0c253725d143fe2bba3b12c927a
Tina Liu <49205802+itingliu@users.noreply.github.com>
2023-05-30 15:53:28 -0700

    Work towards: Stop calling into Foundation's implementation  (#153)
    * Use stdlib's `replacing(:with:)` instead of NSString's `replacingOccurrences(of:with:)`
    * Move internal implementation of `_range(of:anchored:backwards:)` to _FoundationInternals. Update callsites to use this instead of NSString's `range(of:)` implementation.
    Note: we should consider moving to stdlib's `firstRange(of:)` once rdar://109910336 is addressed.
    * Add tests for _range(of:anchored:backwards:)

--- Sources/_FoundationInternals/String/BidirectionalCollection.swift
+++ Sources/_FoundationInternals/String/BidirectionalCollection.swift
@@ -61,4 +61,72 @@ extension BidirectionalCollection {
         }
         return self[startOfNonTrimmedRange ... backIdx]
     }
+
+    // Equal to calling `index(&idx, offsetBy: -other.count)` with just one loop
+    func _index<S: BidirectionalCollection>(_ index: Index, backwardsOffsetByCountOf other: S) -> Index? {
+        var idx = index
+        var otherIdx = other.endIndex
+        while otherIdx > other.startIndex  {
+            guard idx > startIndex else {
+                // other.count > self.count: bail
+                return nil
+            }
+            other.formIndex(before: &otherIdx)
+            formIndex(before: &idx)
+        }
+        return idx
+    }
+
+    func _range<S: BidirectionalCollection>(of other: S, anchored: Bool = false, backwards: Bool = false) -> Range<Index>? where S.Element == Element, Element : Equatable {
+        var result: Range<Index>? = nil
+        var fromLoc: Index
+        var toLoc: Index
+        if backwards {
+            guard let idx = _index(endIndex, backwardsOffsetByCountOf: other) else {
+                // other.count > string.count: bail
+                return nil
+            }
+            fromLoc = idx
+
+            toLoc = anchored ? fromLoc : startIndex
+        } else {
+            fromLoc = startIndex
+            if anchored {
+                toLoc = fromLoc
+            } else {
+                guard let idx = _index(endIndex, backwardsOffsetByCountOf: other) else {
+                    return nil
+                }
+                toLoc = idx
+            }
+        }
+
+        let delta = fromLoc <= toLoc ? 1 : -1
+
+        while true {
+            var str1Index = fromLoc
+            var str2Index = other.startIndex
+
+            while str2Index < other.endIndex && str1Index < endIndex {
+                if self[str1Index] != other[str2Index] {
+                    break
+                }
+                formIndex(after: &str1Index)
+                other.formIndex(after: &str2Index)
+            }
+
+            if str2Index == other.endIndex {
+                result = fromLoc..<str1Index
+                break
+            }
+
+            if fromLoc == toLoc {
+                break
+            }
+
+            formIndex(&fromLoc, offsetBy: delta)
+        }
+
+        return result
+    }
 }
