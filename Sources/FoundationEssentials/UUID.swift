90d50e8cbfa9ba002fde3df3839a5bbd8b33b402
Guoye Zhang <guoye_zhang@apple.com>
2023-07-31 14:05:32 -0700

    (112739802) Fix remaining copyright comments (#213)

--- Sources/FoundationEssentials/UUID.swift
+++ Sources/FoundationEssentials/UUID.swift
@@ -1,6 +1,6 @@
 //===----------------------------------------------------------------------===//
 //
-// This source file is part of the Swift open source project
+// This source file is part of the Swift.org open source project
 //
 // Copyright (c) 2022 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
