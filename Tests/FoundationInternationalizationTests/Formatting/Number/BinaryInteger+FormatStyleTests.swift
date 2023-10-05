1b0e1f5c382b1b9327cfa53d05735ed817709fa2
Wade Tregaskis <wadetregaskis@mac.com>
2023-10-04 17:28:59 -0700

    Updated copyright headers per @glessard's direction (https://github.com/apple/swift-foundation/pull/262#discussion_r1346604109).

--- Tests/FoundationInternationalizationTests/Formatting/Number/BinaryInteger+FormatStyleTests.swift
+++ Tests/FoundationInternationalizationTests/Formatting/Number/BinaryInteger+FormatStyleTests.swift
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 Wade Tregaskis
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
