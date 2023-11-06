6a4a7735ed947c38484bd76090131300edb2f903
Joakim Hassila <jocke@ordo.one>
2023-11-06 18:18:05 +0100

    Bump tools to 5.9 for the benchmarks, so we can depend on macOS .v14 for predicates directly

--- Benchmarks/Benchmarks/Predicates/Predicates.swift
+++ Benchmarks/Benchmarks/Predicates/Predicates.swift
@@ -19,63 +19,56 @@ let benchmarks = {
     Benchmark.defaultConfiguration.maxDuration = .seconds(5)
     Benchmark.defaultConfiguration.scalingFactor = .kilo
     Benchmark.defaultConfiguration.metrics = .arc + [.cpuTotal, .wallClock, .mallocCountTotal, .throughput] // use ARC to see traffic
-//    Benchmark.defaultConfiguration.metrics = [.cpuTotal, .wallClock, .mallocCountTotal, .throughput] // skip ARC as it has some overhead
-//    Benchmark.defaultConfiguration.metrics = .all // Use all metrics to easily see which ones are of interest for this benchmark suite
+//  Benchmark.defaultConfiguration.metrics = [.cpuTotal, .wallClock, .mallocCountTotal, .throughput] // skip ARC as it has some overhead
+//  Benchmark.defaultConfiguration.metrics = .all // Use all metrics to easily see which ones are of interest for this benchmark suite
 
-    if #available(macOS 14, *) {
-        let monster = Monster(name: "Orc", level: 80, hp: 100, mana: 0, weapon: .sword(Sword(p1: 1, p2: 2, p3: 3, p4: 4, p5: 5)))
+    let monster = Monster(name: "Orc", level: 80, hp: 100, mana: 0, weapon: .sword(Sword(p1: 1, p2: 2, p3: 3, p4: 4, p5: 5)))
 
-        var predicateTests : [(String, Predicate<Monster>)] = []
+    var predicateTests : [(String, Predicate<Monster>)] = []
 
-        predicateTests.append(("Predicate #1 - simple 'true' condition", #Predicate<Monster> { monster in
-            true
-        }))
+    predicateTests.append(("Predicate #1 - simple 'true' condition", #Predicate<Monster> { monster in
+        true
+    }))
 
-        predicateTests.append(("Predicate #2 - 1 KeyPath variable condition", #Predicate<Monster> { monster in
-            (monster.level == 80)
-        }))
+    predicateTests.append(("Predicate #2 - 1 KeyPath variable condition", #Predicate<Monster> { monster in
+        (monster.level == 80)
+    }))
 
-        predicateTests.append(("Predicate #3 - 1 KeyPath computed property condition", #Predicate<Monster> { monster in
-            (monster.levelComputed == 80)
-        }))
+    predicateTests.append(("Predicate #3 - 1 KeyPath computed property condition", #Predicate<Monster> { monster in
+        (monster.levelComputed == 80)
+    }))
 
-        predicateTests.append(("Predicate #4 - 1 KeyPath nested computed property condition", #Predicate<Monster> { monster in
-            (monster.weaponP1 == 1)
-        }))
+    predicateTests.append(("Predicate #4 - 1 KeyPath nested computed property condition", #Predicate<Monster> { monster in
+        (monster.weaponP1 == 1)
+    }))
 
-        predicateTests.append(("Predicate #5 - 3 KeyPath nested computed property conditions", #Predicate<Monster> { monster in
-            ((monster.weaponP1 == 1) &&
-             (monster.weaponP2 == 2) &&
-             (monster.weaponP3 == 3))
-        }))
+    predicateTests.append(("Predicate #5 - 3 KeyPath nested computed property conditions", #Predicate<Monster> { monster in
+        ((monster.weaponP1 == 1) &&
+         (monster.weaponP2 == 2) &&
+         (monster.weaponP3 == 3))
+    }))
 
-        // This test disabled, as enabling it will make compilation fail due to:
-        //        .../swift-foundation/Benchmarks/Benchmarks/Predicates/Predicates.swift:17:5: error: the compiler is unable to type-check this expression in reasonable time; try breaking up the expression into distinct sub-expressions
-        //        let benchmarks = {
-        //            ^~~~~~~~~~~~~~
-        //        error: fatalError
-        //        predicateTests.append(("Predicate #6 - 5 KeyPath nested computed property conditions", #Predicate<Monster> { monster in
-        //            ((monster.weaponP1 == 1) &&
-        //             (monster.weaponP2 == 2) &&
-        //             (monster.weaponP3 == 3) &&
-        //             (monster.weaponP4 == 4) &&
-        //             (monster.weaponP5 == 5))
-        //        }))
-        // This is reported in https://github.com/apple/swift/issues/69277
+    // This test disabled, as enabling it will make compilation fail due to https://github.com/apple/swift/issues/69277
+    //      predicateTests.append(("Predicate #6 - 5 KeyPath nested computed property conditions", #Predicate<Monster> { monster in
+    //          ((monster.weaponP1 == 1) &&
+    //           (monster.weaponP2 == 2) &&
+    //           (monster.weaponP3 == 3) &&
+    //           (monster.weaponP4 == 4) &&
+    //           (monster.weaponP5 == 5))
+    //      }))
 
-        predicateTests.forEach { (testDescription, predicate) in
-            Benchmark(testDescription) { benchmark in
-                var matched = 0
+    predicateTests.forEach { (testDescription, predicate) in
+        Benchmark(testDescription) { benchmark in
+            var matched = 0
 
-                for _ in benchmark.scaledIterations {
-                    if try predicate.evaluate(monster) {
-                        matched += 1
-                    }
+            for _ in benchmark.scaledIterations {
+                if try predicate.evaluate(monster) {
+                    matched += 1
                 }
+            }
 
-                guard matched == benchmark.scaledIterations.count else {
-                    fatalError("Internal error: wrong number of matched monsters")
-                }
+            guard matched == benchmark.scaledIterations.count else {
+                fatalError("Internal error: wrong number of matched monsters")
             }
         }
     }
--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "benchmarks",
     platforms: [
-        .macOS("13.3"),
+        .macOS(.v14),
     ],
     dependencies: [
         .package(path: "../"),
