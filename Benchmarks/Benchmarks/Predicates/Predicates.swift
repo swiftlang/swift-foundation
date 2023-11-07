f8c784669bb94716730fc6309a2eb82fb8434aba
ser <122270051+ser-0xff@users.noreply.github.com>
2023-11-08 01:46:14 +0300

    Few benchmarks for predicate evaluation (#291)
    * fix(patch): [sc-3840] Use branch instead of hash to be possible to use from the package-data-model.
    
    * fix(patch): [sc-3840] Refer dependencies using branches to avoid SPM complaining.
    
    * fix(patch): Rename ordo/swift-foundation to ordo/package-swift-foundation
    
    * Bump dependency version.
    
    * fix(patch): Bump dependency version
    
    * Bump dependency version.
    
    * Benchmark for predicates.
    
    * Cosmetic fix.
    
    * Rollback changes in the package.
    
    * Make benchmark as a separate package.
    
    * Change directory structure.
    
    * Some more reasonable benchmark defaults.
    
    * Add conditions with computed properties.
    
    * Cosmetic fix.
    
    * Cosmetic fix.
    
    * Add nested computed property benchmarks
    
    * Update Predicates.swift 
    
    Add standard project header
    
    * Address PR feedback
    
    * Add link to failed compilation
    
    * Bump tools to 5.9 for the benchmarks, so we can depend on macOS .v14 for predicates directly
    
    * Match parent project platforms exactly, reverted required if #available check for macOS 14
    
    * Dynamically calculate test number to make it easier to add additional tests (the test number is mostly useful for filtering with --filter when wanting to run a specific test)
    
    * Use zero numbering for test cases
    
    * Simplify naming and remove counters, makes filtering benchmarks easier for manual workflows, e.g. swift package benchmark --filter predicateKeypathPropertyCondition
    
    * Add multivariate predicate test
    
    * Polish multivariate test
    
    * Use same naming for multivariate test as for single.
    
    * Fix naming for variadic tests, decrease run time to 3 seconds as we still get enough samples and stable results
    
    ---------
    
    Co-authored-by: Joakim Hassila <jocke@ordo.one>

---
+++ Benchmarks/Benchmarks/Predicates/Predicates.swift
@@ -0,0 +1,103 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2022-2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Benchmark
+import func Benchmark.blackHole
+import FoundationEssentials
+
+let benchmarks = {
+    Benchmark.defaultConfiguration.maxIterations = 1_000_000_000
+    Benchmark.defaultConfiguration.maxDuration = .seconds(3)
+    Benchmark.defaultConfiguration.scalingFactor = .kilo
+    Benchmark.defaultConfiguration.metrics = .arc + [.cpuTotal, .wallClock, .mallocCountTotal, .throughput] // use ARC to see traffic
+//  Benchmark.defaultConfiguration.metrics = [.cpuTotal, .wallClock, .mallocCountTotal, .throughput] // skip ARC as it has some overhead
+//  Benchmark.defaultConfiguration.metrics = .all // Use all metrics to easily see which ones are of interest for this benchmark suite
+    if #available(macOS 14, *) {
+
+        let monster = Monster(name: "Orc", level: 80, hp: 100, mana: 0, weapon: .sword(Sword(p1: 1, p2: 2, p3: 3, p4: 4, p5: 5)))
+
+        var predicateTests : [(String, Predicate<Monster>)] = []
+
+        predicateTests.append(("predicateTrivialCondition", #Predicate<Monster> { monster in
+            true
+        }))
+
+        predicateTests.append(("predicateKeypathPropertyCondition", #Predicate<Monster> { monster in
+            (monster.level == 80)
+        }))
+
+        predicateTests.append(("predicateKeypathComputedPropertyCondition", #Predicate<Monster> { monster in
+            (monster.levelComputed == 80)
+        }))
+
+        predicateTests.append(("predicateKeypathNestedComputedPropertyCondition", #Predicate<Monster> { monster in
+            (monster.weaponP1 == 1)
+        }))
+
+        predicateTests.append(("predicateThreeKeypathNestedComputedPropertyCondition", #Predicate<Monster> { monster in
+            ((monster.weaponP1 == 1) &&
+             (monster.weaponP2 == 2) &&
+             (monster.weaponP3 == 3))
+        }))
+
+    // This test disabled, as enabling it will make compilation fail due to https://github.com/apple/swift/issues/69277
+    //      predicateTests.append(("predicateFiveKeypathNestedComputedPropertyCondition", #Predicate<Monster> { monster in
+    //          ((monster.weaponP1 == 1) &&
+    //           (monster.weaponP2 == 2) &&
+    //           (monster.weaponP3 == 3) &&
+    //           (monster.weaponP4 == 4) &&
+    //           (monster.weaponP5 == 5))
+    //      }))
+
+        predicateTests.forEach { (testDescription, predicate) in
+            Benchmark(testDescription) { benchmark in
+                var matched = 0
+
+                for _ in benchmark.scaledIterations {
+                    if try predicate.evaluate(monster) {
+                        matched += 1
+                    }
+                }
+
+                guard matched == benchmark.scaledIterations.count else {
+                    fatalError("Internal error: wrong number of matched monsters")
+                }
+            }
+        }
+
+        var variadicPredicateTests : [(String, Predicate<Monster, Monster>)] = []
+        let monster2 = Monster(name: "Orc", level: 80, hp: 100, mana: 0, weapon: .sword(Sword(p1: 1, p2: 2, p3: 3, p4: 4, p5: 5)))
+
+        variadicPredicateTests.append(("predicateVariadicThreeKeypathNestedComputedPropertyCondition",
+                                       #Predicate<Monster, Monster> { monster, monster2 in
+            ((monster.weaponP1 == 1) &&
+             (monster.weaponP2 == 2) &&
+             (monster2.weaponP2 == 2))
+        }))
+
+        variadicPredicateTests.forEach { (testDescription, predicate) in
+            Benchmark(testDescription) { benchmark in
+                var matched = 0
+
+                for _ in benchmark.scaledIterations {
+                    if try predicate.evaluate(monster, monster2) {
+                        matched += 1
+                    }
+                }
+
+                guard matched == benchmark.scaledIterations.count else {
+                    fatalError("Internal error: wrong number of matched monsters")
+                }
+            }
+        }
+    }
+}
---
+++ Benchmarks/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "benchmarks",
+    platforms: [.macOS("13.3"), .iOS("16.4"), .tvOS("16.4"), .watchOS("9.4")], // Should match parent project
+    dependencies: [
+        .package(path: "../"),
+        .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.11.1"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "PredicateBenchmarks",
+            dependencies: [
+                .product(name: "FoundationEssentials", package: "swift-foundation"),
+                .product(name: "Benchmark", package: "package-benchmark"),
+            ],
+            path: "Benchmarks/Predicates",
+            plugins: [
+                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+            ]
+        ),
+    ]
+)
