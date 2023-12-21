81b3030ec9ce5562c3ab9d5c0084903b1ad9ffba
Joakim Hassila <jocke@ordo.one>
2023-12-21 12:45:04 +0100

    Try using macros separately

--- Package.swift
+++ Package.swift
@@ -13,78 +13,17 @@ let availabilityMacros: [SwiftSetting] = [
 ].map { .enableExperimentalFeature("AvailabilityMacro=\($0)") }
 
 let package = Package(
-    name: "FoundationPreview",
+    name: "FoundationPreviewMacros",
     platforms: [.macOS("13.3"), .iOS("16.4"), .tvOS("16.4"), .watchOS("9.4")],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "FoundationPreview", targets: ["FoundationPreview"]),
-        .library(name: "FoundationEssentials", targets: ["FoundationEssentials"]),
-        .library(name: "FoundationInternationalization", targets: ["FoundationInternationalization"]),
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/apple/swift-collections",
-            revision: "d8003787efafa82f9805594bc51100be29ac6903"), // on release/1.1
-        .package(
-            url: "https://github.com/apple/swift-foundation-icu",
-            exact: "0.0.4"),
+        .library(name: "FoundationMacrosLinux", targets: ["FoundationMacrosLinux"]),
+    ],    dependencies: [
         .package(
             url: "https://github.com/apple/swift-syntax.git",
             from: "509.0.2")
     ],
     targets: [
-        // Foundation (umbrella)
-        .target(
-            name: "FoundationPreview",
-            dependencies: [
-                "FoundationEssentials",
-                "FoundationInternationalization",
-            ],
-            path: "Sources/Foundation"),
-
-        // _CShims (Internal)
-        .target(name: "_CShims",
-                cSettings: [.define("_CRT_SECURE_NO_WARNINGS",
-                                    .when(platforms: [.windows]))]),
-
-        // TestSupport (Internal)
-        .target(name: "TestSupport", dependencies: [
-            "FoundationEssentials",
-            "FoundationInternationalization",
-        ], swiftSettings: availabilityMacros),
-
-        // FoundationEssentials
-        .target(
-          name: "FoundationEssentials",
-          dependencies: [
-            "_CShims",
-            "FoundationMacros",
-            .product(name: "_RopeModule", package: "swift-collections"),
-          ],
-          swiftSettings: [
-            .enableExperimentalFeature("VariadicGenerics"),
-            .enableExperimentalFeature("AccessLevelOnImport")
-          ] + availabilityMacros
-        ),
-        .testTarget(name: "FoundationEssentialsTests", dependencies: [
-            "TestSupport",
-            "FoundationEssentials"
-        ], swiftSettings: availabilityMacros),
-
-        // FoundationInternationalization
-        .target(
-            name: "FoundationInternationalization",
-            dependencies: [
-                .target(name: "FoundationEssentials"),
-                .target(name: "_CShims"),
-                .product(name: "FoundationICU", package: "swift-foundation-icu")
-            ],
-            swiftSettings: [
-                .enableExperimentalFeature("AccessLevelOnImport")
-            ] + availabilityMacros
-        ),
-        
-        // FoundationMacros
+        .target(name: "FoundationMacrosLinux", dependencies: ["FoundationMacros"]),
         .macro(
             name: "FoundationMacros",
             dependencies: [
@@ -102,19 +41,9 @@ let package = Package(
         .testTarget(
             name: "FoundationMacrosTests",
             dependencies: [
-                "FoundationMacros",
-                "TestSupport"
+                "FoundationMacros"
             ],
             swiftSettings: availabilityMacros
         )
     ]
 )
-
-#if canImport(RegexBuilder)
-package.targets.append(contentsOf: [
-    .testTarget(name: "FoundationInternationalizationTests", dependencies: [
-        "TestSupport",
-        "FoundationInternationalization"
-    ], swiftSettings: availabilityMacros),
-])
-#endif
