e59a6e023c2088c75b4b887fc9bfb90e03d0299d
Jeremy Schonfeld <jschonfeld@apple.com>
2024-01-04 13:22:38 -0800

    Provide manual name override for local swift-foundation in the benchmark package

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -6,14 +6,14 @@ let package = Package(
     name: "benchmarks",
     platforms: [.macOS("13.3"), .iOS("16.4"), .tvOS("16.4"), .watchOS("9.4")], // Should match parent project
     dependencies: [
-        .package(path: "../"),
+        .package(name: "swift-foundation-local", path: "../"),
         .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.11.1"),
     ],
     targets: [
         .executableTarget(
             name: "PredicateBenchmarks",
             dependencies: [
-                .product(name: "FoundationEssentials", package: "swift-foundation"),
+                .product(name: "FoundationEssentials", package: "swift-foundation-local"),
                 .product(name: "Benchmark", package: "package-benchmark"),
             ],
             path: "Benchmarks/Predicates",
