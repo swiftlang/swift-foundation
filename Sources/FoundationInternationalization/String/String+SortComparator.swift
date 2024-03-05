ea66a6dba9f156ecebd543298260476d1b804406
Tony Parker <anthony.parker@apple.com>
2024-03-05 12:26:38 -0800

    Fix lexical sorting for SortComparator

--- Sources/FoundationInternationalization/String/String+SortComparator.swift
+++ Sources/FoundationInternationalization/String/String+SortComparator.swift
@@ -144,7 +144,12 @@ extension String {
         public func compare(_ lhs: String, _ rhs: String) -> ComparisonResult {
 #if FOUNDATION_FRAMEWORK
             // https://github.com/apple/swift-foundation/issues/284
-            return lhs.compare(rhs, options: options, locale: Locale.current).withOrder(order)
+            
+            if isLocalized {
+                return lhs.compare(rhs, options: options, locale: Locale.current).withOrder(order)
+            } else {
+                return lhs.compare(rhs, options: options).withOrder(order)
+            }
 #else
             // TODO: Until compare(_:options:locale:) is ported to FoundationInternationalization, only support unlocalized
             return lhs.compare(rhs, options: options).withOrder(order)
