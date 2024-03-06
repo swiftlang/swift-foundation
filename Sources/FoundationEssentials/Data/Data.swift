6d2eddfc96a428fb1bcb35c636635ba5ed123780
Tony Parker <anthony.parker@apple.com>
2024-03-06 14:45:51 -0800

    Update Data.swift framework side builds; add base64 benchmark (#466)

--- Sources/FoundationEssentials/Data/Data.swift
+++ Sources/FoundationEssentials/Data/Data.swift
@@ -2608,6 +2608,36 @@ public struct Data : Equatable, Hashable, RandomAccessCollection, MutableCollect
         }
     }
 
+    // MARK: - Range
+    
+#if FOUNDATION_FRAMEWORK
+    /// Find the given `Data` in the content of this `Data`.
+    ///
+    /// - parameter dataToFind: The data to be searched for.
+    /// - parameter options: Options for the search. Default value is `[]`.
+    /// - parameter range: The range of this data in which to perform the search. Default value is `nil`, which means the entire content of this data.
+    /// - returns: A `Range` specifying the location of the found data, or nil if a match could not be found.
+    /// - precondition: `range` must be in the bounds of the Data.
+    public func range(of dataToFind: Data, options: Data.SearchOptions = [], in range: Range<Index>? = nil) -> Range<Index>? {
+        let nsRange : NSRange
+        if let r = range {
+            nsRange = NSRange(location: r.lowerBound - startIndex, length: r.upperBound - r.lowerBound)
+        } else {
+            nsRange = NSRange(location: 0, length: count)
+        }
+        let result = _representation.withInteriorPointerReference {
+            let opts = NSData.SearchOptions(rawValue: options.rawValue)
+            return $0.range(of: dataToFind, options: opts, in: nsRange)
+        }
+        if result.location == NSNotFound {
+            return nil
+        }
+        return (result.location + startIndex)..<((result.location + startIndex) + result.length)
+    }
+#else
+    // TODO: Implement range(of:options:in:) for Foundation package.
+#endif
+
     // MARK: -
     //
 
@@ -2671,6 +2701,14 @@ extension Data {
         /// Modify the decoding algorithm so that it ignores unknown non-Base-64 bytes, including line ending characters.
         public static let ignoreUnknownCharacters = Base64DecodingOptions(rawValue: 1 << 0)
     }
+}
+#else
+@available(macOS 10.10, iOS 8.0, watchOS 2.0, tvOS 9.0, *)
+extension Data {
+    // These types are typealiased to the `NSData` options for framework builds only.
+    public typealias SearchOptions = NSData.SearchOptions
+    public typealias Base64EncodingOptions = NSData.Base64EncodingOptions
+    public typealias Base64DecodingOptions = NSData.Base64DecodingOptions
 }
 #endif //!FOUNDATION_FRAMEWORK
 
