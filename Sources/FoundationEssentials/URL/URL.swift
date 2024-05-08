e07817cbd6a87a7a02f32a3f043468bb9af6bb28
Saleem Abdulrasool <compnerd@compnerd.org>
2024-05-09 06:04:19 +0900

    FoundationEssentials: recover from backslide on Windows (#593)
    This new helper was added but did not support windows as `stat` is a
    Unix operation and the Windows equivalent is more nuanced when you are
    trying to minimise IO overheads. Add a Windows specific path to repair
    the build on Windows.

--- Sources/FoundationEssentials/URL/URL.swift
+++ Sources/FoundationEssentials/URL/URL.swift
@@ -1991,7 +1991,15 @@ extension URL {
 
 #if !NO_FILESYSTEM
     private static func isDirectory(_ path: String) -> Bool {
-        return FileManager.default._fileStat(path)?.isDirectory ?? false
+#if !FOUNDATION_FRAMEWORK
+        var isDirectory: Bool = false
+        _ = FileManager.default.fileExists(atPath: path, isDirectory: &isDirectory)
+        return isDirectory
+#else
+        var isDirectory: ObjCBool = false
+        _ = FileManager.default.fileExists(atPath: path, isDirectory: &isDirectory)
+        return isDirectory.boolValue
+#endif
     }
 #endif // !NO_FILESYSTEM
 
