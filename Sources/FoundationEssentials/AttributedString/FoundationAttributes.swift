6654fd13e722a0d6e8aec27c9c878470b4f80cbd
Jeremy Schonfeld <jschonfeld@apple.com>
2024-06-03 17:18:07 -0700

    Fix build failure

--- Sources/FoundationEssentials/AttributedString/FoundationAttributes.swift
+++ Sources/FoundationEssentials/AttributedString/FoundationAttributes.swift
@@ -12,6 +12,10 @@
 
 // MARK: Attribute Scope
 
+#if FOUNDATION_FRAMEWORK
+internal import Foundation_Private.NSAttributedString
+#endif
+
 @available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
 extension AttributeScopes {
     public var foundation: FoundationAttributes.Type { FoundationAttributes.self }
