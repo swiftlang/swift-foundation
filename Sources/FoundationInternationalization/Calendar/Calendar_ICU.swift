062c4155b034b62bb7dae150d2894077df95f63f
Evan Wilde <etceterawilde@gmail.com>
2024-08-08 15:44:50 -0700

    Get FoundationInternationalization building
    Adding the missing Musl imports to get FoundationInternationalization
    building for the static SDK.

--- Sources/FoundationInternationalization/Calendar/Calendar_ICU.swift
+++ Sources/FoundationInternationalization/Calendar/Calendar_ICU.swift
@@ -18,6 +18,8 @@ import FoundationEssentials
 import Android
 #elseif canImport(Glibc)
 import Glibc
+#elseif canImport(Musl)
+import Musl
 #elseif canImport(CRT)
 import CRT
 #elseif canImport(Darwin)
