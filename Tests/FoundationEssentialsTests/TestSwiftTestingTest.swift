8e46de96e0c1b0e23be76ec4eedd0805298a94ae
Jeremy Schonfeld <jschonfeld@apple.com>
2025-01-02 11:06:42 -0500

    Add simple swift testing test

---
+++ Tests/FoundationEssentialsTests/TestSwiftTestingTest.swift
@@ -0,0 +1,15 @@
+//
+//  TestSwiftTestingTest.swift
+//  swift-foundation
+//
+//  Created by Jeremy Schonfeld on 1/2/25.
+//
+
+import Testing
+
+struct TestSwiftTestingTests {
+    @Test
+    func swiftTestingTest() {
+        #expect(1 == 1)
+    }
+}
