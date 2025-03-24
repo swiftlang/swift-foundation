a7217301074ac2b1da6d2c451e9e59f97ab1b621
Fabian Fett <fabianfett@apple.com>
2025-03-24 14:23:09 +0100

    Reapply #1160

--- Sources/FoundationEssentials/JSON/JSONDecoder.swift
+++ Sources/FoundationEssentials/JSON/JSONDecoder.swift
@@ -679,7 +679,7 @@ extension JSONDecoderImpl: Decoder {
             var data: Data?
             if isSimple {
                 data = withBuffer(for: region) { buffer, _ in
-                    Data(decodingBase64: buffer)
+                    try? Base64.decode(bytes: buffer)
                 }
             }
             if data == nil {
