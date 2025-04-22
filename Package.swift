351b77a7769bf2d422178a1e1f24d922b20362e1
chloe-yeo <chloelokyee_yeo@apple.com>
2025-04-22 11:11:23 -0700

    exclude CMake from Package.swift

--- Package.swift
+++ Package.swift
@@ -133,7 +133,8 @@ let package = Package(
             "CMakeLists.txt",
             "ProcessInfo/CMakeLists.txt",
             "FileManager/CMakeLists.txt",
-            "URL/CMakeLists.txt"
+            "URL/CMakeLists.txt",
+            "ProgressReporter/CMakeLists.txt"
           ],
           cSettings: [
             .define("_GNU_SOURCE", .when(platforms: [.linux]))
@@ -195,7 +196,8 @@ let package = Package(
                 "Locale/CMakeLists.txt",
                 "Calendar/CMakeLists.txt",
                 "CMakeLists.txt",
-                "Predicate/CMakeLists.txt"
+                "Predicate/CMakeLists.txt",
+                "ProgressReporter/CMakeLists.txt"
             ],
             cSettings: wasiLibcCSettings,
             swiftSettings: [
