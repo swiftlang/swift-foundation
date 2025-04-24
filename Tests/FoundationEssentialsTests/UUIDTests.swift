d9ffe2ff4b7a782cab7f65f57027c6b98109d66d
Franz Busch <f.busch@apple.com>
2025-04-24 20:57:09 +0200

    Proposal to generate `UUID`s using `RandomNumberGenerator`s
    This PR adds a proposal to generate `UUID's` using `RandomNumberGenerator`s

--- Tests/FoundationEssentialsTests/UUIDTests.swift
+++ Tests/FoundationEssentialsTests/UUIDTests.swift
@@ -115,4 +115,23 @@ final class UUIDTests : XCTestCase {
         XCTAssertFalse(uuid2 > uuid1)
         XCTAssertTrue(uuid2 == uuid1)
     }
+    
+    func testRandomVersionAndVariant() {
+        var generator = SystemRandomNumberGenerator()
+        for _ in 0..<10000 {
+            let uuid = UUID.random(using: &generator)
+            XCTAssertEqual(uuid.versionNumber, 0b0100)
+            XCTAssertEqual(uuid.varint, 0b10)
+        }
+    }
+}
+
+extension UUID {
+    fileprivate var versionNumber: Int {
+        Int(self.uuid.6 >> 4)
+    }
+    
+    fileprivate var varint: Int {
+        Int(self.uuid.8 >> 6 & 0b11)
+    }
 }
