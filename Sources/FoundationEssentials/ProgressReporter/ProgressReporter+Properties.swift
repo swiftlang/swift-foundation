185c5f956f95e20e314ecf8ed5d4edbb3d65f6f6
chloe-yeo <chloelokyee_yeo@apple.com>
2025-04-28 18:37:05 -0700

    add default value static var to Property protocol

--- Sources/FoundationEssentials/ProgressReporter/ProgressReporter+Properties.swift
+++ Sources/FoundationEssentials/ProgressReporter/ProgressReporter+Properties.swift
@@ -15,31 +15,43 @@ extension ProgressReporter {
     public struct Properties: Sendable {
         public var totalFileCount: TotalFileCount.Type { TotalFileCount.self }
         public struct TotalFileCount: Sendable, Property {
+            public static var defaultValue: Int { return 0 }
+            
             public typealias T = Int
         }
         
         public var completedFileCount: CompletedFileCount.Type { CompletedFileCount.self }
         public struct CompletedFileCount: Sendable, Property {
+            public static var defaultValue: Int { return 0 }
+             
             public typealias T = Int
         }
         
         public var totalByteCount: TotalByteCount.Type { TotalByteCount.self }
         public struct TotalByteCount: Sendable, Property {
+            public static var defaultValue: Int64 { return 0 }
+            
             public typealias T = Int64
         }
         
         public var completedByteCount: CompletedByteCount.Type { CompletedByteCount.self }
         public struct CompletedByteCount: Sendable, Property {
+            public static var defaultValue: Int64 { return 0 }
+            
             public typealias T = Int64
         }
         
         public var throughput: Throughput.Type { Throughput.self }
         public struct Throughput: Sendable, Property {
+            public static var defaultValue: Int64 { return 0 }
+            
             public typealias T = Int64
         }
         
         public var estimatedTimeRemaining: EstimatedTimeRemaining.Type { EstimatedTimeRemaining.self }
         public struct EstimatedTimeRemaining: Sendable, Property {
+            public static var defaultValue: Duration { return Duration.seconds(0) }
+            
             public typealias T = Duration
         }
     }
