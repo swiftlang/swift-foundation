e0dfddce49455f45e92dff73dabcf2f50632408a
Jeremy Schonfeld <jschonfeld@apple.com>
2025-06-11 14:34:00 -0700

    Convert SortComparator tests

--- Tests/FoundationInternationalizationTests/StringSortComparatorTests.swift
+++ Tests/FoundationInternationalizationTests/StringSortComparatorTests.swift
@@ -10,43 +10,48 @@
 //
 //===----------------------------------------------------------------------===//
 
-#if canImport(TestSupport)
-import TestSupport
-#endif
+import Testing
 
-#if FOUNDATION_FRAMEWORK
-@testable import Foundation
-#else
+#if canImport(FoundationEssentials)
 @testable import FoundationEssentials
 @testable import FoundationInternationalization
-#endif // FOUNDATION_FRAMEWORK
+#else
+@testable import Foundation
+#endif
 
-class StringSortComparatorTests: XCTestCase {
+@Suite("String SortComparator")
+private struct StringSortComparatorTests {
+    @Test func compareOptionsDescriptor() {
+        let compareOptions = String.Comparator(options: [.numeric])
+        #expect(
+            compareOptions.compare("ttestest005", "test2") ==
+            "test005".compare("test2", options: [.numeric]))
+        #expect(
+            compareOptions.compare("test2", "test005") ==
+            "test2".compare("test005", options: [.numeric]))
+    }
+    
 #if FOUNDATION_FRAMEWORK
     // TODO: Until we support String.compare(_:options:locale:) in FoundationInternationalization, only support unlocalized comparisons
     // https://github.com/apple/swift-foundation/issues/284
-    func test_locale() {
+    @Test func locale() {
         let swedishComparator = String.Comparator(options: [], locale: Locale(identifier: "sv"))
-        XCTAssertEqual(swedishComparator.compare("ă", "ã"), .orderedAscending)
-        XCTAssertEqual(swedishComparator.locale, Locale(identifier: "sv"))
+        #expect(swedishComparator.compare("ă", "ã") == .orderedAscending)
+        #expect(swedishComparator.locale, Locale(identifier: "sv"))
     }
     
-    func test_nil_locale() {
+    @Test func nilLocale() {
         let swedishComparator = String.Comparator(options: [], locale: nil)
-        XCTAssertEqual(swedishComparator.compare("ă", "ã"), .orderedDescending)
+        #expect(swedishComparator.compare("ă", "ã") == .orderedDescending)
     }
     
-    func test_standard_localized() throws {
-        // This test is only verified to work with en
-        guard Locale.current.language.languageCode == .english else {
-            throw XCTSkip("Test only verified to work with English as current language")
-        }
-        
+    @Test(.enabled(if: Locale.current.language.languageCode == .english, "Test only verified to work with English as current language"))
+    func standardLocalized() throws {
         let localizedStandard = String.StandardComparator.localizedStandard
-        XCTAssertEqual(localizedStandard.compare("ă", "ã"), .orderedAscending)
+        #expect(localizedStandard.compare("ă", "ã") == .orderedAscending)
         
         let unlocalizedStandard = String.StandardComparator.lexical
-        XCTAssertEqual(unlocalizedStandard.compare("ă", "ã"), .orderedDescending)
+        #expect(unlocalizedStandard.compare("ă", "ã") == .orderedDescending)
     }
 #endif
 }
