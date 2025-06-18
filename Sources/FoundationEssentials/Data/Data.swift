1fe187bbf3702b40806b551261713aadfc4f8135
Guillaume Lessard <guillaume.lessard@apple.com>
2025-06-17 17:37:54 -0700

    [Windows] Clean up workarounds and enable `Span` (#1349)
    * Remove some workarounds
    
    Remove conditional compilation statements put in place to work around a mismatch in the compiler version used by the Windows CI.
    
    * Remove more workarounds
    
    Remove conditional compilation statements put in place to work around a mismatch in the compiler version used by the Windows CI.

--- Sources/FoundationEssentials/Data/Data.swift
+++ Sources/FoundationEssentials/Data/Data.swift
@@ -2203,7 +2203,6 @@ public struct Data : Equatable, Hashable, RandomAccessCollection, MutableCollect
         return try _representation.withUnsafeBytes(body)
     }
 
-#if compiler(>=6.2) && $LifetimeDependence
     @available(FoundationSpan 6.2, *)
     public var bytes: RawSpan {
         @lifetime(borrow self)
@@ -2239,9 +2238,7 @@ public struct Data : Equatable, Hashable, RandomAccessCollection, MutableCollect
             return _overrideLifetime(span, borrowing: self)
         }
     }
-#endif
 
-#if compiler(>=5.9) && $InoutLifetimeDependence && $LifetimeDependenceMutableAccessors
     @available(FoundationSpan 6.2, *)
     public var mutableBytes: MutableRawSpan {
         @lifetime(&self)
@@ -2301,7 +2298,6 @@ public struct Data : Equatable, Hashable, RandomAccessCollection, MutableCollect
 #endif
         }
     }
-#endif // $InoutLifetimeDependence && $LifetimeDependenceMutableAccessors
 
     @_alwaysEmitIntoClient
     public func withContiguousStorageIfAvailable<ResultType>(_ body: (_ buffer: UnsafeBufferPointer<UInt8>) throws -> ResultType) rethrows -> ResultType? {
@@ -2976,7 +2972,6 @@ extension Data : Codable {
 }
 
 // TODO: remove once _overrideLifetime is public in the standard library
-#if compiler(>=6.2) && $LifetimeDependence
 /// Unsafely discard any lifetime dependency on the `dependent` argument. Return
 /// a value identical to `dependent` with a lifetime dependency on the caller's
 /// borrow scope of the `source` argument.
@@ -3008,9 +3003,7 @@ internal func _overrideLifetime<
 ) -> T {
   dependent
 }
-#endif
 
-#if compiler(>=5.9) && $InoutLifetimeDependence && $LifetimeDependenceMutableAccessors
 /// Unsafely discard any lifetime dependency on the `dependent` argument.
 /// Return a value identical to `dependent` with a lifetime dependency
 /// on the caller's exclusive borrow scope of the `source` argument.
@@ -3027,4 +3020,3 @@ internal func _overrideLifetime<
 ) -> T {
   dependent
 }
-#endif // $InoutLifetimeDependence && $LifetimeDependenceMutableAccessors
