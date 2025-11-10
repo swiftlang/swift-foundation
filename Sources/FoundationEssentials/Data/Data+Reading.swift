b9bdc6ff329b06c5881481f23687e23a5757bcf0
Guillaume Lessard <guillaume.lessard@apple.com>
2025-11-10 08:46:09 -0800

    Use updated spelling for lifetime annotations.

--- Sources/FoundationEssentials/Data/Data+Reading.swift
+++ Sources/FoundationEssentials/Data/Data+Reading.swift
@@ -193,7 +193,7 @@ struct ReadBytesResult {
 }
 
 #if os(Windows)
-@lifetime(pBuffer: copy pBuffer)
+@_lifetime(pBuffer: copy pBuffer)
 private func read(from hFile: HANDLE, at path: PathOrURL,
                   into pBuffer: inout OutputRawSpan,
                   chunkSize dwChunk: Int = 4096, progress bProgress: Bool)
