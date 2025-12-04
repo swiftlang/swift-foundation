db58f285fc3162a567a41c28d29a975a6203c1c1
Guillaume Lessard <guillaume.lessard@apple.com>
2025-12-04 14:19:52 -0800

    Generalize Data’s `withUnsafeBytes()` functions
    Tweak a test (silence a warning)
    
    Generalize Data’s `withUnsafeMutableBytes` functions
    
    Test generalized functions
    
    Only compile old ABI for FOUNDATION_FRAMEWORK
    
    Verify the old ABI
    
    Spell `rethrows` as `throws` in ABI-compatibility functions
    
    Improve test by using a more specific #expect
    
    Add a test suite specifically for legacy ABI tests
    
    Generalize `withContiguousStorageIfAvailable` as well

--- Sources/FoundationEssentials/Data/Representations/Data+LegacyRepresentation.swift
+++ Sources/FoundationEssentials/Data/Representations/Data+LegacyRepresentation.swift
@@ -233,7 +233,7 @@ extension Data {
                         }
                     }
                 }
-                
+
                 if let rep = apply(&self, newValue) {
                     self = rep
                 }
@@ -304,8 +304,8 @@ extension Data {
             }
         }
 
-        @inlinable // This is @inlinable as a generic, trivially forwarding function.
-        func withUnsafeBytes<Result>(_ apply: (UnsafeRawBufferPointer) throws -> Result) rethrows -> Result {
+        @_alwaysEmitIntoClient
+        func withUnsafeBytes<E, Result: ~Copyable>(_ apply: (UnsafeRawBufferPointer) throws(E) -> Result) throws(E) -> Result {
             switch self {
             case .empty:
                 let empty = InlineData()
@@ -318,9 +318,18 @@ extension Data {
                 return try slice.withUnsafeBytes(apply)
             }
         }
-        
-        @inlinable // This is @inlinable as a generic, trivially forwarding function.
-        mutating func withUnsafeMutableBytes<Result>(_ apply: (UnsafeMutableRawBufferPointer) throws -> Result) rethrows -> Result {
+
+#if FOUNDATION_FRAMEWORK
+        @abi(func withUnsafeBytes<R>(_: (UnsafeRawBufferPointer) throws -> R) throws -> R)
+        @_spi(FoundationLegacyABI)
+        @usableFromInline
+        internal func _legacy_withUnsafeBytes<ResultType>(_ body: (UnsafeRawBufferPointer) throws -> ResultType) throws -> ResultType {
+            try withUnsafeBytes(body)
+        }
+#endif // FOUNDATION_FRAMEWORK
+
+        @_alwaysEmitIntoClient
+        mutating func withUnsafeMutableBytes<E, Result: ~Copyable>(_ apply: (UnsafeMutableRawBufferPointer) throws(E) -> Result) throws(E) -> Result {
             switch self {
             case .empty:
                 var empty = InlineData()
@@ -338,7 +347,16 @@ extension Data {
                 return try slice.withUnsafeMutableBytes(apply)
             }
         }
-        
+
+#if FOUNDATION_FRAMEWORK
+        @abi(mutating func withUnsafeMutableBytes<R>(_: (UnsafeMutableRawBufferPointer) throws -> R) throws -> R)
+        @_spi(FoundationLegacyABI)
+        @usableFromInline
+        internal mutating func _legacy_withUnsafeMutableBytes<ResultType>(_ body: (UnsafeMutableRawBufferPointer) throws -> ResultType) throws -> ResultType {
+            try withUnsafeMutableBytes(body)
+        }
+#endif // FOUNDATION_FRAMEWORK
+
         @usableFromInline // This is not @inlinable as it is a non-trivial, non-generic function.
         func enumerateBytes(_ block: (_ buffer: UnsafeBufferPointer<UInt8>, _ byteIndex: Index, _ stop: inout Bool) -> Void) {
             switch self {
