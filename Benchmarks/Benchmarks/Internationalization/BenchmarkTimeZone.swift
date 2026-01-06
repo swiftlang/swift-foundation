ad6dde1e9de92d46b2207acbd43866ac4d9e63d7
Tina L <49205802+itingliu@users.noreply.github.com>
2026-01-05 16:54:49 -0800

    Use `_TimeZoneGMTICU` for timezones whose identifier take the form of "GMT/UTC+<offset>"  (#1657)
    * Add tests and benchmark
    
    * Use `_TimeZoneGMTICU` for timezones whose identifier take the form of "GMT/UTC+<offset>" such as `"GMT+8"` for performance reasons.
    
    Also update to uatimezone API for localizing names.
    
    166054881
    
    * Update benchmark
    
    * Add a comment to clarify it's safe to close uatimezone

--- Benchmarks/Benchmarks/Internationalization/BenchmarkTimeZone.swift
+++ Benchmarks/Benchmarks/Internationalization/BenchmarkTimeZone.swift
@@ -28,10 +28,17 @@ let benchmarks = {
 #endif
 
 let testDates = {
-    var now = Date.now
+    let seeds: [Date] = [
+        Date(timeIntervalSince1970: -2827137600), // 1880-05-30T12:00:00
+        Date(timeIntervalSince1970: 0),
+        Date.now,
+        Date(timeIntervalSince1970: 26205249600) // 2800-05-30
+    ]
     var dates: [Date] = []
-    for i in 0...10000 {
-        dates.append(Date(timeInterval: Double(i * 3600), since: now))
+    for seed in seeds {
+        for i in 0...2000 {
+            dates.append(Date(timeInterval: Double(i * 3600), since: seed))
+        }
     }
     return dates
 }()
@@ -71,6 +78,68 @@ func timeZoneBenchmarks() {
             blackHole(t)
         }
     }
+
+    guard let gmtPlus8 = TimeZone(identifier: "GMT+8") else {
+        fatalError("unexpected failure when creating time zone")
+    }
+
+    let locale = Locale(identifier: "jp_JP")
+    let gmtOffsetTimeZoneConfiguration = Benchmark.Configuration(scalingFactor: .mega)
+
+    var gmtOffsetTimeZoneNames = (0...14).map { "GMT+\($0)" }
+    gmtOffsetTimeZoneNames.append(contentsOf: (0...12).map{ "GMT-\($0)" })
+
+    Benchmark("GMTOffsetTimeZone-creation", configuration: gmtOffsetTimeZoneConfiguration) { benchmark in
+        for name in gmtOffsetTimeZoneNames {
+            guard let gmtPlus = TimeZone(identifier: name) else {
+                fatalError("unexpected failure when creating time zone: \(name)")
+            }
+            blackHole(gmtPlus)
+        }
+    }
+
+    Benchmark("GMTOffsetTimeZone-secondsFromGMT", configuration: gmtOffsetTimeZoneConfiguration) { benchmark in
+        for d in testDates {
+            let secondsFromGMT = gmtPlus8.secondsFromGMT(for: d)
+            blackHole(secondsFromGMT)
+        }
+    }
+
+    Benchmark("GMTOffsetTimeZone-abbreviation", configuration: gmtOffsetTimeZoneConfiguration) { benchmark in
+        for d in testDates {
+            let abbreviation = gmtPlus8.abbreviation(for: d)
+            blackHole(abbreviation)
+        }
+    }
+
+    Benchmark("GMTOffsetTimeZone-nextDaylightSavingTimeTransition", configuration: gmtOffsetTimeZoneConfiguration) { benchmark in
+        for d in testDates {
+            let nextDST = gmtPlus8.nextDaylightSavingTimeTransition(after: d)
+            blackHole(nextDST)
+        }
+    }
+
+    Benchmark("GMTOffsetTimeZone-daylightSavingTimeOffsets", configuration: gmtOffsetTimeZoneConfiguration) { benchmark in
+        for d in testDates {
+            let dstOffset = gmtPlus8.daylightSavingTimeOffset(for: d)
+            blackHole(dstOffset)
+        }
+    }
+
+    Benchmark("GMTOffsetTimeZone-isDaylightSavingTime", configuration: gmtOffsetTimeZoneConfiguration) { benchmark in
+        for d in testDates {
+            let isDST = gmtPlus8.isDaylightSavingTime(for: d)
+            blackHole(isDST)
+        }
+    }
+
+    Benchmark("GMTOffsetTimeZone-localizedNames", configuration: gmtOffsetTimeZoneConfiguration) { benchmark in
+        for style in [TimeZone.NameStyle.generic, .standard, .shortGeneric, .shortStandard, .daylightSaving, .shortDaylightSaving] {
+            let localizedName = gmtPlus8.localizedName(for: style, locale: locale)
+            blackHole(localizedName)
+        }
+    }
+
 }
 
 
