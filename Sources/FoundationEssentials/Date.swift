4bc9073e01bfbc19fb37e1b11bbf7f59f5de78ae
Stephen Canon <scanon@apple.com>
2026-09-09 10:45:17 -0400

    Pull Date <= and >= into 6.4.x marked backDeployed.

--- Sources/FoundationEssentials/Date.swift
+++ Sources/FoundationEssentials/Date.swift
@@ -195,6 +195,19 @@ public struct Date : Comparable, Hashable, Equatable, Sendable {
     public static func >(lhs: Date, rhs: Date) -> Bool {
         return lhs.timeIntervalSinceReferenceDate > rhs.timeIntervalSinceReferenceDate
     }
+  
+    // These two symbols are backDeployed; when building with an older SDK users still get the default implementation from Comparable, which isn't quite right (because of NaN), but this lets us make it do the right thing when they recompile with an up-to-date SDK.
+    /// Returns true if the left hand `Date` is earlier in time than or equal to the right hand `Date`.
+    @backDeployed(before: FoundationPreview 6.4.2)
+    public static func <=(lhs: Date, rhs: Date) -> Bool {
+        return lhs.timeIntervalSinceReferenceDate <= rhs.timeIntervalSinceReferenceDate
+    }
+
+    /// Returns true if the left hand `Date` is later in time than or equal to the right hand `Date`.
+    @backDeployed(before: FoundationPreview 6.4.2)
+    public static func >=(lhs: Date, rhs: Date) -> Bool {
+        return lhs.timeIntervalSinceReferenceDate >= rhs.timeIntervalSinceReferenceDate
+    }
 
     /// Returns a date with a specified amount of time added to it.
     ///
