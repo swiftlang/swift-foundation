db0a731408fef462fc7c455ecd3cdcca44147a88
Chloe Yeo <chloelokyee_yeo@apple.com>
2026-09-23 18:23:56 -0700

    adopt suggestion: use SwiftTesting

--- Tests/FoundationEssentialsTests/BuiltInUnicodeScalarSetTests.swift
+++ Tests/FoundationEssentialsTests/BuiltInUnicodeScalarSetTests.swift
@@ -161,23 +161,19 @@ private struct BuiltInUnicodeScalarSetTests {
         setContainsScalar(controlAndFormatter, "\u{D0000}", false)
     }
 
-    @Test func partialBitmapMatchesFullPlanePrefix() {
-        let types: [BuiltInUnicodeScalarSet.SetType] = [.letter, .control, .illegal, .whitespace, .newline, .whitespaceAndNewline]
-        for type in types {
-            let set = BuiltInUnicodeScalarSet(type: type)
-            for plane in 0...16 {
-                for isInverted in [false, true] {
-                    let full = Array(set.bitmap(forPlane: plane, isInverted: isInverted))
-                    for count in [1, 10, 16, 8187, 8188] {
-                        let partial = Data(capacity: count) { output in
-                            output.withOutputSpan(of: UInt8.self) { typedOutput in
-                                set.appendBitmap(forPlane: plane, isInverted: isInverted, to: &typedOutput)
-                            }
-                        let partialResult = set.bitmap(forPlane: plane, isInverted: isInverted, into: &partialSpan) { $0 = $1 }
-                        #expect(partialResult == fullResult)
-                        #expect(partial == Array(full.prefix(count)))
-                        #expect(Array(partial) == Array(full.prefix(count)))
+    @Test(arguments: [BuiltInUnicodeScalarSet.SetType.letter, .control, .illegal, .whitespace, .newline, .whitespaceAndNewline])
+    func partialBitmapMatchesFullPlanePrefix(type: BuiltInUnicodeScalarSet.SetType) {
+        let set = BuiltInUnicodeScalarSet(type: type)
+        for plane in 0...16 {
+            for isInverted in [false, true] {
+                let full = Array(set.bitmap(forPlane: plane, isInverted: isInverted))
+                for count in [1, 10, 16, 8187, 8188] {
+                    let partial = Data(capacity: count) { output in
+                        output.withOutputSpan(of: UInt8.self) { typedOutput in
+                            set.appendBitmap(forPlane: plane, isInverted: isInverted, to: &typedOutput)
+                        }
                     }
+                    #expect(Array(partial) == Array(full.prefix(count)))
                 }
             }
         }
